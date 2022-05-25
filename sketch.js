b2e020c78c98536620756de918e6bf14a05377f3
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2022-05-25 17:56:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 var roadImage;
 var carImage;
-var boxImage, coinImage, coneImage, logImage; 
+var boxImage, coinImage, coneImage, logImage;
 
-var gameState=1;
-var PLAY=1;
-
-
+var gameState = 1;
+var PLAY = 1, box;
 
 
-function preload(){
+
+
+function preload() {
   roadImage = loadImage("Road.png");
   carImage = loadImage("CarImage.png");
   boxImage = loadImage("BoxImage.png");
@@ -21,92 +21,95 @@ function preload(){
 function setup() {
   //create the canvas and adjust the window sizes to suit the device 
   createCanvas(windowWidth, windowHeight);
- //putting an background in
- background(0);
+  //putting an background in
+  background(0);
   //creating road
-  road=createSprite(width/2,200);
+  road = createSprite(width / 2, 200);
   road.addImage(roadImage);
-  road.velocityY=7;
-  road.scale=4;
+  road.velocityY = 7;
+  road.scale = 4;
 
   //creating car
-  car=createSprite(751,786);
+  car = createSprite(windowWidth / 2, windowHeight - 100);
   car.addImage(carImage);
-  car.scale=0.3;
+  car.scale = 0.1;
+  car.debug = true
 
 }
 
 function draw() {
-  
+  box = Math.round(random(1, 2))
   //codes for gamestate = PLAY
-  if(gameState===PLAY){
-    
+  if (gameState === PLAY) {
+
     //putting an background in
     background(0);
 
     //code to reset the road
-    if(road.y > height){
-     road.y = height/2;
-   }
+    if (road.y > height) {
+      road.y = height / 2;
+    }
 
     //code for moving car to the right when right arrow is pressed 
-    if(keyDown("right")){
+    if (keyDown("right")) {
       car.x = 1179
       car.y = 786
     }
 
     //code for moving car to the left when left arrow is pressed 
-    if(keyDown("left")){
+    if (keyDown("left")) {
       car.x = 751
       car.y = 786
     }
-
-    if(frameCount % 50 == 0){
-      if(box == 1){
+    if (frameCount % 50 == 0) {
+      console.log(box)
+      if (box == 1) {
         createBox();
       } else if (box == 2) {
         createCoin();
-      } else{
+      } else {
         createlog();
-      }   }
+      }
+    }
 
     //creating the sprites 
     drawSprites();
-    
+
   }
- 
+
 }
 
-function createBox(){
-  if(World.frameCount % 320 == 0){
-  box = createSprite(random(750,1215),1, 10, 10);  
+function createBox() {
+  console.log("executed")
+  box = createSprite(random(0,windowWidth / 2), 1, 10, 10);
   box.addImage(boxImage);
-  box.scale=0.3
-  box.velocityY=3;
-  box.lifetime=150;
-  }
-
-
-  }
-
-
-function createCoin(){
- if(World.frameCount % 530 == 0){
-  coin = createSprite(random(750,1215),1, 10, 10);  
-  coin.addImage(coinImage);
- coin.scale=0.3
-  coin.velocityY=3;
-  coin.lifetime=150;
- }
-
-
-  }
-function createlog(){
-  if(World.frameCount % 530 == 0){
-  log = createSprite(random(750,1215),1, 10, 10);  
- log.addImage(logImage);
-  log.scale=0.3
- log.velocityY=3;
- log.lifetime=150;
+  box.scale = 0.1
+  box.velocityY = 3;
+  log.lifetime = 500;
+  box.debug = true
 }
+
+
+
+
+
+function createCoin() {
+  
+    coin = createSprite(random(0,windowWidth / 2), 1, 10, 10);
+    coin.addImage(coinImage);
+    coin.scale = 0.1
+    coin.velocityY = 3;
+    coin.lifetime = 500;
+  
+
+
+}
+function createlog() {
+
+    log = createSprite(random(0,windowWidth / 2), 1, 10, 10);
+    log.addImage(logImage);
+    log.scale = 0.3
+    log.velocityY = 3;
+    log.lifetime = 500;
+  
 }
